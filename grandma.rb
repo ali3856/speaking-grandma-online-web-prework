6fcca90e361c830c4eb7ccc0bdb086777bfad546
witty-banner-1950 <asimsali95@gmail.com>
2019-04-25 02:33:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,10 @@
 
 def speak_to_grandma (phrase)
   if phrase == "I love you grandma".upcase
-    puts "I love you too".upcase
-    
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == "".upcase
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end  
 end
